a8ce6a3070b7658b08ea1a0028a7f7505193d351
choibohyun <320fleur@gmail.com>
2024-05-04 15:51:43 +0900

    Initial commit

---
+++ membershipTDD/src/main/java/com/banbi/membershiptdd/MembershipTddApplication.java
@@ -0,0 +1,13 @@
+package com.banbi.membershiptdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembershipTddApplication.class, args);
+    }
+
+}
---
+++ membershipTDD/src/test/java/com/banbi/membershiptdd/MembershipTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banbi.membershiptdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
